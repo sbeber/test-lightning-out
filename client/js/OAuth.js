19eea56b2866486e21df740ee55e5fe205740041
sbeber <sbeber41@hotmail.fr>
2017-10-17 11:25:53 +0200

    Update OAuth.js

--- client/js/OAuth.js
+++ client/js/OAuth.js
@@ -68,7 +68,7 @@ function oauthAutoConnect(){
 	
 	var xhr = new XMLHttpRequest();
 	xhr.open('POST', 'https://trailhead-sbeber-dev-ed.my.salesforce.com/services/oauth2/token',true);
-	xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+	/*xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 	
 	xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
 	xhr.setRequestHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization,X-Authorization'); 

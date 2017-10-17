1345d1a287992f79c0b5d97cbd45d3740b53c4c0
sbeber <sbeber41@hotmail.fr>
2017-10-17 11:59:52 +0200

    Update Server.js

--- Server.js
+++ Server.js
@@ -53,6 +53,10 @@ app.get('/oauthcallback.html' ,  function(req,res,next) {
 app.get('/Main*' ,   function(req,res,next) {
     res.sendfile('views/Main.html');
 } );
+
+app.get('/getToken' ,   function(req,res,next) {
+    res.redirect('https://login.salesforce.com/services/oauth2/token');
+} );
  
 
 app.listen(app.get('port'), function () {

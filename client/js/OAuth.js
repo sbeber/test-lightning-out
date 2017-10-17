977958c698767e7d62f1bfa7f1c45a3051fa9ac3
sbeber <sbeber41@hotmail.fr>
2017-10-17 10:18:34 +0200

    Update OAuth.js

--- client/js/OAuth.js
+++ client/js/OAuth.js
@@ -69,6 +69,7 @@ function oauthAutoConnect(){
 	var xhr = new XMLHttpRequest();
 	xhr.open('POST', loginUrl + 'services/oauth2/token');
 	xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+	xhr.withCredentials = true;
 	xhr.send(body);
 	
 	xhr.addEventListener('readystatechange', function() {
